04c940bca529b3138c1fe2e80751d6edac347d64
Daniel Prilik <danielprilik@gmail.com>
2016-04-10 00:41:58 -0400

    Hopefully this polyfill fixes iOS errors

--- main.js
+++ main.js
@@ -300,7 +300,29 @@ function init () {
     init_graph();
 }
 
-
-
-
 window.onload = init;
+
+// An Object.assign polyfill
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
